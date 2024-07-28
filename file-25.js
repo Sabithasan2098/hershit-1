95e9c0c2a53693ae4e79dfc91dbbfccf93a0dab8
Sabit hasan <sabithasan2098@gmail.com>
2024-07-28 16:31:20 +0600

    second commit

--- file-25.js
+++ file-25.js
@@ -1,12 +1,29 @@
-function Name(firstName,lastName){
-    this.firstName = firstName
-    this.lastName = lastName
-}
-Name.prototype.about = function(){
-    return `He is mr ${this.firstName} ${this.lastName} `
-}
-
-
-const user1 =new Name("sabit","hasan")
-
- console.log(user1.about());
+// কনস্ট্রাক্টর ফাংশন
+function CreateObj(name, age, passion, job) {
+    this.name = name;
+    this.age = age;
+    this.passion = passion;
+    this.job = job;
+  }
+  
+  // প্রোটোটাইপ মেথড যোগ করা
+ CreateObj.prototype.about = function() {
+    return `My name is ${this.name} and my age is ${this.age}`;
+  };
+  
+ CreateObj.prototype.is18 = function() {
+    return this.age >= 18 ? "true" : "false";
+  };
+  
+ CreateObj.prototype.myJob = function() {
+    return `I am continious ${this.job}`;
+  };
+  
+  // নতুন অবজেক্ট তৈরি করা
+  const user1 = new CreateObj("Sabit", 17, "job hunter", "learning");
+  
+  // প্রোটোটাইপ মেথড কল করা
+  console.log(user1.about());  // Output: My name is Sabit and my age is 17
+  console.log(user1.is18());   // Output: false
+  console.log(user1.myJob());  // Output: I am continious learning
+  
